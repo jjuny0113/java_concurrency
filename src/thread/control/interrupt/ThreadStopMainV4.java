08d987db465177105df21db2f4d91b0785684391
jjuny0113 <junnyletsgo@gmail.com>
2024-08-07 01:04:13 +0900

    스레드 제어와 생명주기2

---
+++ src/thread/control/interrupt/ThreadStopMainV4.java
@@ -0,0 +1,38 @@
+package thread.control.interrupt;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class ThreadStopMainV4 {
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+       Thread thread =  new Thread(task, "work");
+        thread.start();
+        sleep(100);
+        log("작업 중단 지시 thread.interrupt()");
+        thread.interrupt();
+        log("work 스레드 인터럽트 상태1 = " + thread.isInterrupted());
+    }
+
+    static class MyTask implements Runnable {
+
+        @Override
+        public void run(){
+
+            while (!Thread.interrupted()) { // 인트럽트 상태 변경o
+                log("작업중");
+
+            }
+            log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());
+            try {
+                log("작업정리");
+                Thread.sleep(1000);
+                log("자원종료");
+            } catch (InterruptedException e) {
+                log("자원 정리 실패 - 자원 정리 중 인터럽트 발생");
+                log("work 스레드 인트럽트 상태=" + Thread.currentThread().isInterrupted());
+            }
+            log("작업 종료");
+        }
+    }
+}
